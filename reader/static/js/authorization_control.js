b957d07e74ffb7d183655ac180dccca94d42cfd2
Vlad <vladprzlv@yandex.ru>
2022-10-29 00:22:56 +0500

    Личный кабинет

---
+++ reader/static/js/authorization_control.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var btn = document.querySelector('.btn-authorization'),
+        loader = document.querySelector('.loader'),
+        check = document.querySelector('.check');
+    
+    btn.addEventListener('click', function () {
+      loader.classList.add('active');    
+    });
+   
+    loader.addEventListener('animationend', function() {
+      check.classList.add('active'); 
+    });
+  });
+  
